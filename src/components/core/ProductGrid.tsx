5a341b56ff3353d1ecb6f5cc4631e1dd76f1d4d7
DJTX97 <the.10.of.one@gmail.com>
2023-09-05 13:38:02 +0300

    feat: added endless scroll on home page

--- src/components/core/ProductGrid.tsx
+++ src/components/core/ProductGrid.tsx
@@ -10,28 +10,69 @@ interface ProductGridProps {
   products: Product[];
 }
 
+const initialItemsToShow = 8;
+const batchSize = 8;
+const threshold = 100; // Adjust this value as per your requirements
+
 export default function ProductGrid({ products }: ProductGridProps) {
-
   const [amount, setAmount] = useAtom(counter);
-
   const [criteria] = useAtom(selectedFilter);
-  const [results, setResults] = useState(products);
+  const [filteredProducts, setFilteredProducts] = useState(products);
+  const [results, setResults] = useState(
+    filteredProducts.slice(0, initialItemsToShow)
+  );
+  const [currentBatch, setCurrentBatch] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setAmount(1);
-  }, [])
+  }, []);
 
   useEffect(() => {
-    setResults(productFilter(products, criteria));
+    setFilteredProducts(productFilter(products, criteria));
+    setCurrentBatch(1);
+  }, [products, criteria]);
 
-  }, [criteria])
+  useEffect(() => {
+    setResults(filteredProducts.slice(0, initialItemsToShow));
+    setCurrentBatch(1);
+  }, [filteredProducts]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition =
+        window.innerHeight + document.documentElement.scrollTop;
+      const documentHeight = document.documentElement.offsetHeight;
+      const isReachedThreshold = scrollPosition >= documentHeight - threshold;
+
+      if (isReachedThreshold && !isLoading) {
+        setIsLoading(true);
+        const startIndex = currentBatch * batchSize;
+        const endIndex = startIndex + batchSize;
+        const nextBatch = filteredProducts.slice(startIndex, endIndex);
+
+        // Simulate loading delay (replace with your actual data fetching logic)
+        setTimeout(() => {
+          setResults((prevResults) => [...prevResults, ...nextBatch]);
+          setCurrentBatch((prevBatchNumber) => prevBatchNumber + 1);
+          setIsLoading(false);
+        }, 1000);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [filteredProducts, currentBatch, isLoading]);
 
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 2xl:gap-0">
       {results.map((product, index) => (
         <ProductCard key={index} product={product} />
       ))}
+      {isLoading && <div>Loading...</div>} {/* Show a loading indicator */}
     </div>
   );
 }

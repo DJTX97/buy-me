dfbe2b55b2906bfb51b1ed22fe4447e70aec374c
DJTX97 <the.10.of.one@gmail.com>
2023-09-07 15:42:38 +0300

    style: improved footer

--- src/components/core/Footer.tsx
+++ src/components/core/Footer.tsx
@@ -3,6 +3,7 @@ import { socialMedia } from "@/utils/externalNav";
 export default function Footer() {
   return (
     <div className="bg-slate-500">
+      <div className="text-center pt-4 text-3xl text-white font-semibold">Contact us on social media!</div>
       <div className="flex justify-center gap-3 p-5">
         {socialMedia.map((item, index) => (
           <a key={index} href={item.link} target="_blank">

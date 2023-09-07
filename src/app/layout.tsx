b3364bf8c8238be8ed3dad34c134172a23952829
DJTX97 <the.10.of.one@gmail.com>
2023-09-07 16:00:50 +0300

    feat: improved metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,10 @@ import type { Metadata } from "next";
 import Header from "@/components/core/Header";
 import Footer from "@/components/core/Footer";
 
-
 export const metadata: Metadata = {
   title: "BuyMe",
-  description: "Best place to buy your next PC",
+  description: "Best ecommerce general store site for top cheap products!",
+  keywords: ["buy", "sale", "best of the year", "best offers", "trending", "product", "best products", "cheap", "ecommerce", "store", "discount"],
   icons: {
     icon: "/assets/favicon/brand.png",
   }, //path to favicon
@@ -19,13 +19,11 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-
-        <body>
-          <Header />
-          <div className="p-10 2xl:px-40 2xl:py-20 bg-slate-400">{children}</div>
-          <Footer />
-        </body>
-
+      <body>
+        <Header />
+        <div className="p-10 2xl:px-40 2xl:py-20 bg-slate-400">{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }

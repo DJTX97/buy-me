d49d9af686f4f7c1c92396d8d781ed61333e8f82
DJTX97 <the.10.of.one@gmail.com>
2023-09-11 13:38:48 +0300

    fix: fixed deployment errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
-export default function notFound() {
+export default function NotFound() {
   useEffect(() => {
     redirect("/");
   }, [])
